7dbafc323e41d14ba06deb524879e0c583432368
Iván <ivan2006.gg@gmail.com>
2026-04-05 11:42:59 +0200

    Añadir animaciones y sliders a las tarjetas de las peliculas y hacer que las peliculas que salgan sean en ESP o ING

--- frontend/src/services/tmdbService.js
+++ frontend/src/services/tmdbService.js
@@ -2,10 +2,12 @@ const TMDB_KEY = import.meta.env.VITE_TMDB_KEY;
 const TMDB_BASE = 'https://api.themoviedb.org/3';
 export const TMDB_IMG = 'https://image.tmdb.org/t/p/w500';
 
-const fetchFromTMDB = async (endpoint) => {
-    const res = await fetch(`${TMDB_BASE}${endpoint}?api_key=${TMDB_KEY}&language=es-ES&page=1`);
+const fetchFromTMDB = async (endpoint, params = "") => {
+    const res = await fetch(`${TMDB_BASE}${endpoint}?api_key=${TMDB_KEY}&language=es-ES&region=ES&page=1${params}`);
     const data = await res.json();
-    return data.results.slice(0, 4);
+    return (data.results || [])
+        .filter(movie => movie.original_language === 'es' || movie.original_language === 'en')
+        .slice(0, 20);
 };
 
 export const getPeliculasPopulares = () => fetchFromTMDB('/movie/popular');
